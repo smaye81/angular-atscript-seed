640c4f435cf97262bf875d4a1bfb3cd0f519d27c
Steve Ayers <smaye81@gmail.com>
2014-11-08 22:13:48 -0500

    Added comments to Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,13 +4,21 @@ var traceur = require('gulp-traceur');
 var concat = require('gulp-concat');
 var connect = require("gulp-connect");
 
+// Traceur options
 var TRACEUR_OPTIONS = require('./config').traceur;
+
+// Base path to RTTS library
 var ASSERT_BASE = './node_modules/rtts-assert/dist/';
 
-var PATH = {
-    SRC: ['!./app/traceur-runtime.js', './app/*.ats', './app/modules/**/*.ats', './app/*.js']
-};
+// Path for source files to run through Traceur
+var SRC_PATH = ['!./app/traceur-runtime.js', './app/*.ats', './app/modules/**/*.ats', './app/*.js'];
 
+/**
+ * Returns the path to the RTTS Assert library based on the moduleType used in Traceur config
+ *
+ * @param moduleType
+ * @returns {string}
+ */
 var getAssertPath = function (moduleType) {
     var dir;
     if (moduleType === "register") {
@@ -29,13 +37,14 @@ var getAssertPath = function (moduleType) {
     return ASSERT_BASE + dir + '/assert.js';
 };
 
+// Watch files and run through traceur when they change
 gulp.task('watch', function () {
-    gulp.watch([PATH.SRC], ['traceur']);
+    gulp.watch([SRC_PATH], ['traceur']);
 });
 
-/* Not working with current version of gulp-traceur */
+// Not working with current version of gulp-traceur
 gulp.task('traceur', function () {
-    return gulp.src(PATH.SRC, {base: './app'})
+    return gulp.src(SRC_PATH, {base: './app'})
         .pipe(sourcemaps.init())
         .pipe(traceur(TRACEUR_OPTIONS))
         .pipe(concat('bundle.js'))
@@ -43,18 +52,22 @@ gulp.task('traceur', function () {
         .pipe(gulp.dest('app/dist'));
 });
 
+// Main task to copy the basic runtime (Traceur and RTTS)
 gulp.task('copy-runtime', ['copy-assert', 'copy-traceur']);
 
+// Copies the RTTS Assert library to main app directory to run through Traceur with source code
 gulp.task('copy-assert', function () {
     gulp.src(getAssertPath(TRACEUR_OPTIONS.modules))
         .pipe(gulp.dest('./app'));
 });
 
+// Copies the Traceur runtime to the main app directory
 gulp.task('copy-traceur', function () {
     gulp.src([traceur.RUNTIME_PATH])
         .pipe(gulp.dest('./app'));
 });
 
+// Start a local dev server
 gulp.task('connect', function () {
 
     // Uses gulp-connect plugin to start up a server
